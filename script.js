94e2da06d7f3d83333115a7afe8f084c726170bc
unknown <farheenirfan12nov@gmail.com git config --global user.name Farheenirfan123>
2024-01-29 04:42:11 -0800

    js table

---
+++ script.js
@@ -0,0 +1,37 @@
+
+
+// for( let table; i<=10; i++){
+//    document.write(` x ${table} = ${i*table} <br>`)
+//    }
+//    let cars= ["bwm" , "corolla" , "audi" , "ferrari"]
+//    console.log(cars[0]);
+
+let names = ["Aisha", "Tooba", "Aliza", "Zara", "Hafsa", "Maryam"];
+let a;
+
+for(a=0; a<=5; a++){
+    document.write(`Welcome dear ${names[a]} <br>`);
+}
+
+// let num1 =prompt("enter  number") ;
+// let num2 = prompt("enter  number") ;
+
+// let result = num1 / num2;
+
+// if(Number.isFinite(result)){
+//     document.write("correct");
+// }
+// else{
+//     document.write("math error");
+// }
+ 
+let user ="Farheen";
+
+let result = user.toUppercase;
+
+document.write(result);
+
+
+
+
+
